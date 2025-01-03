747ea5ce8140e1b0975538d38821acaf3377bb87
Alvarjur <aalvarojur@gmail.com>
2025-01-03 14:46:05 +0100

    creacion de mazos

---
+++ Programacion/mazos.py
@@ -0,0 +1,160 @@
+esp48 = {
+    "001": {"literal": "As de Oros", "value": 1, "priority": 4, "realValue": 1}, #Oros
+    "002": {"literal": "Dos de Oros", "value": 2, "priority": 4, "realValue": 2},
+    "003": {"literal": "Tres de Oros", "value": 3, "priority": 4, "realValue": 3},
+    "004": {"literal": "Cuatro de Oros", "value": 4, "priority": 4, "realValue": 4},
+    "005": {"literal": "Cinco de Oros", "value": 5, "priority": 4, "realValue": 5},
+    "006": {"literal": "Seis de Oros", "value": 6, "priority": 4, "realValue": 6},
+    "007": {"literal": "Siete de Oros", "value": 7, "priority": 4, "realValue": 7},
+    "008": {"literal": "Ocho de Oros", "value": 8, "priority": 4, "realValue": 0.5}, 
+    "009": {"literal": "Nueve de Oros", "value": 9, "priority": 4, "realValue": 0.5},
+    "010": {"literal": "Sota de Oros", "value": 10, "priority": 4, "realValue": 0.5},
+    "011": {"literal": "Caballo de Oros", "value": 11, "priority": 4, "realValue": 0.5},
+    "012": {"literal": "Rey de Oros", "value": 12, "priority": 4, "realValue": 0.5},
+
+    "013": {"literal": "As de Copas", "value": 1, "priority": 3, "realValue": 1}, #Copas
+    "014": {"literal": "Dos de Copas", "value": 2, "priority": 3, "realValue": 2},
+    "015": {"literal": "Tres de Copas", "value": 3, "priority": 3, "realValue": 3},
+    "016": {"literal": "Cuatro de Copas", "value": 4, "priority": 3, "realValue": 4},
+    "017": {"literal": "Cinco de Copas", "value": 5, "priority": 3, "realValue": 5},
+    "018": {"literal": "Seis de Copas", "value": 6, "priority": 3, "realValue": 6},
+    "019": {"literal": "Siete de Copas", "value": 7, "priority": 3, "realValue": 7},
+    "020": {"literal": "Ocho de Copas", "value": 8, "priority": 3, "realValue": 0.5},
+    "021": {"literal": "Nueve de Copas", "value": 9, "priority": 3, "realValue": 0.5},
+    "022": {"literal": "Sota de Copas", "value": 10, "priority": 3, "realValue": 0.5},
+    "023": {"literal": "Caballo de Copas", "value": 11, "priority": 3, "realValue": 0.5},
+    "024": {"literal": "Rey de Copas", "value": 12, "priority": 3, "realValue": 0.5},
+
+    "025": {"literal": "As de Espadas", "value": 1, "priority": 2, "realValue": 1}, #Espadas
+    "026": {"literal": "Dos de Espadas", "value": 2, "priority": 2, "realValue": 2},
+    "027": {"literal": "Tres de Espadas", "value": 3, "priority": 2, "realValue": 3},
+    "028": {"literal": "Cuatro de Espadas", "value": 4, "priority": 2, "realValue": 4},
+    "029": {"literal": "Cinco de Espadas", "value": 5, "priority": 2, "realValue": 5},
+    "030": {"literal": "Seis de Espadas", "value": 6, "priority": 2, "realValue": 6},
+    "031": {"literal": "Siete de Espadas", "value": 7, "priority": 2, "realValue": 7},
+    "032": {"literal": "Ocho de Espadas", "value": 8, "priority": 2, "realValue": 0.5},
+    "033": {"literal": "Nueve de Espadas", "value": 9, "priority": 2, "realValue": 0.5},
+    "034": {"literal": "Sota de Espadas", "value": 10, "priority": 2, "realValue": 0.5},
+    "035": {"literal": "Caballo de Espadas", "value": 11, "priority": 2, "realValue": 0.5},
+    "036": {"literal": "Rey de Espadas", "value": 12, "priority": 2, "realValue": 0.5},
+
+    "037": {"literal": "As de Bastos", "value": 1, "priority": 1, "realValue": 1}, #Bastos
+    "038": {"literal": "Dos de Bastos", "value": 2, "priority": 1, "realValue": 2},
+    "039": {"literal": "Tres de Bastos", "value": 3, "priority": 1, "realValue": 3},
+    "040": {"literal": "Cuatro de Bastos", "value": 4, "priority": 1, "realValue": 4},
+    "041": {"literal": "Cinco de Bastos", "value": 5, "priority": 1, "realValue": 5},
+    "042": {"literal": "Seis de Bastos", "value": 6, "priority": 1, "realValue": 6},
+    "043": {"literal": "Siete de Bastos", "value": 7, "priority": 1, "realValue": 7},
+    "044": {"literal": "Ocho de Bastos", "value": 8, "priority": 1, "realValue": 0.5},
+    "045": {"literal": "Nueve de Bastos", "value": 9, "priority": 1, "realValue": 0.5},
+    "046": {"literal": "Sota de Bastos", "value": 10, "priority": 1, "realValue": 0.5},
+    "047": {"literal": "Caballo de Bastos", "value": 11, "priority": 1, "realValue": 0.5},
+    "048": {"literal": "Rey de Bastos", "value": 12, "priority": 1, "realValue": 0.5},
+}
+
+esp40 = {
+    "001": {"literal": "As de Oros", "value": 1, "priority": 4, "realValue": 1}, #Oros
+    "002": {"literal": "Dos de Oros", "value": 2, "priority": 4, "realValue": 2},
+    "003": {"literal": "Tres de Oros", "value": 3, "priority": 4, "realValue": 3},
+    "004": {"literal": "Cuatro de Oros", "value": 4, "priority": 4, "realValue": 4},
+    "005": {"literal": "Cinco de Oros", "value": 5, "priority": 4, "realValue": 5},
+    "006": {"literal": "Seis de Oros", "value": 6, "priority": 4, "realValue": 6},
+    "007": {"literal": "Siete de Oros", "value": 7, "priority": 4, "realValue": 7},
+    "008": {"literal": "Sota de Oros", "value": 10, "priority": 4, "realValue": 0.5},
+    "009": {"literal": "Caballo de Oros", "value": 11, "priority": 4, "realValue": 0.5},
+    "010": {"literal": "Rey de Oros", "value": 12, "priority": 4, "realValue": 0.5},
+
+    "011": {"literal": "As de Copas", "value": 1, "priority": 3, "realValue": 1}, #Copas
+    "012": {"literal": "Dos de Copas", "value": 2, "priority": 3, "realValue": 2},
+    "013": {"literal": "Tres de Copas", "value": 3, "priority": 3, "realValue": 3},
+    "014": {"literal": "Cuatro de Copas", "value": 4, "priority": 3, "realValue": 4},
+    "015": {"literal": "Cinco de Copas", "value": 5, "priority": 3, "realValue": 5},
+    "016": {"literal": "Seis de Copas", "value": 6, "priority": 3, "realValue": 6},
+    "017": {"literal": "Siete de Copas", "value": 7, "priority": 3, "realValue": 7},
+    "018": {"literal": "Sota de Copas", "value": 10, "priority": 3, "realValue": 0.5},
+    "019": {"literal": "Caballo de Copas", "value": 11, "priority": 3, "realValue": 0.5},
+    "020": {"literal": "Rey de Copas", "value": 12, "priority": 3, "realValue": 0.5},
+
+    "021": {"literal": "As de Espadas", "value": 1, "priority": 2, "realValue": 1}, #Espadas
+    "022": {"literal": "Dos de Espadas", "value": 2, "priority": 2, "realValue": 2},
+    "023": {"literal": "Tres de Espadas", "value": 3, "priority": 2, "realValue": 3},
+    "024": {"literal": "Cuatro de Espadas", "value": 4, "priority": 2, "realValue": 4},
+    "025": {"literal": "Cinco de Espadas", "value": 5, "priority": 2, "realValue": 5},
+    "026": {"literal": "Seis de Espadas", "value": 6, "priority": 2, "realValue": 6},
+    "027": {"literal": "Siete de Espadas", "value": 7, "priority": 2, "realValue": 7},
+    "028": {"literal": "Sota de Espadas", "value": 10, "priority": 2, "realValue": 0.5},
+    "029": {"literal": "Caballo de Espadas", "value": 11, "priority": 2, "realValue": 0.5},
+    "030": {"literal": "Rey de Espadas", "value": 12, "priority": 2, "realValue": 0.5},
+
+    "031": {"literal": "As de Bastos", "value": 1, "priority": 1, "realValue": 1}, #Bastos
+    "032": {"literal": "Dos de Bastos", "value": 2, "priority": 1, "realValue": 2},
+    "033": {"literal": "Tres de Bastos", "value": 3, "priority": 1, "realValue": 3},
+    "034": {"literal": "Cuatro de Bastos", "value": 4, "priority": 1, "realValue": 4},
+    "035": {"literal": "Cinco de Bastos", "value": 5, "priority": 1, "realValue": 5},
+    "036": {"literal": "Seis de Bastos", "value": 6, "priority": 1, "realValue": 6},
+    "037": {"literal": "Siete de Bastos", "value": 7, "priority": 1, "realValue": 7},
+    "038": {"literal": "Sota de Bastos", "value": 10, "priority": 1, "realValue": 0.5},
+    "039": {"literal": "Caballo de Bastos", "value": 11, "priority": 1, "realValue": 0.5},
+    "040": {"literal": "Rey de Bastos", "value": 12, "priority": 1, "realValue": 0.5},
+}
+
+poker = {
+    "001": {"literal": "As de Corazones", "value": 1, "priority": 4, "realValue": 1}, #Corazones
+    "002": {"literal": "Dos de Corazones", "value": 2, "priority": 4, "realValue": 2},
+    "003": {"literal": "Tres de Corazones", "value": 3, "priority": 4, "realValue": 3},
+    "004": {"literal": "Cuatro de Corazones", "value": 4, "priority": 4, "realValue": 4},
+    "005": {"literal": "Cinco de Corazones", "value": 5, "priority": 4, "realValue": 5},
+    "006": {"literal": "Seis de Corazones", "value": 6, "priority": 4, "realValue": 6},
+    "007": {"literal": "Siete de Corazones", "value": 7, "priority": 4, "realValue": 7},
+    "008": {"literal": "Ocho de Corazones", "value": 8, "priority": 4, "realValue": 0.5},
+    "009": {"literal": "Nueve de Corazones", "value": 9, "priority": 4, "realValue": 0.5},
+    "010": {"literal": "Diez de Corazones", "value": 10, "priority": 4, "realValue": 0.5},
+    "011": {"literal": "Sota de Corazones", "value": 11, "priority": 4, "realValue": 0.5},
+    "012": {"literal": "Reina de Corazones", "value": 12, "priority": 4, "realValue": 0.5},
+    "013": {"literal": "Rey de Corazones", "value": 13, "priority": 4, "realValue": 0.5},
+
+    "014": {"literal": "As de Diamantes", "value": 1, "priority": 3, "realValue": 1}, #Diamantes
+    "015": {"literal": "Dos de Diamantes", "value": 2, "priority": 3, "realValue": 2},
+    "016": {"literal": "Tres de Diamantes", "value": 3, "priority": 3, "realValue": 3},
+    "017": {"literal": "Cuatro de Diamantes", "value": 4, "priority": 3, "realValue": 4},
+    "018": {"literal": "Cinco de Diamantes", "value": 5, "priority": 3, "realValue": 5},
+    "019": {"literal": "Seis de Diamantes", "value": 6, "priority": 3, "realValue": 6},
+    "020": {"literal": "Siete de Diamantes", "value": 7, "priority": 3, "realValue": 7},
+    "021": {"literal": "Ocho de Diamantes", "value": 8, "priority": 3, "realValue": 0.5},
+    "022": {"literal": "Nueve de Diamantes", "value": 9, "priority": 3, "realValue": 0.5},
+    "023": {"literal": "Diez de Diamantes", "value": 10, "priority": 3, "realValue": 0.5},
+    "024": {"literal": "Sota de Diamantes", "value": 11, "priority": 3, "realValue": 0.5},
+    "025": {"literal": "Reina de Diamantes", "value": 12, "priority": 3, "realValue": 0.5},
+    "026": {"literal": "Rey de Diamantes", "value": 13, "priority": 3, "realValue": 0.5},
+
+    "027": {"literal": "As de Treboles", "value": 1, "priority": 2, "realValue": 1}, #Tréboles
+    "028": {"literal": "Dos de Treboles", "value": 2, "priority": 2, "realValue": 2},
+    "029": {"literal": "Tres de Treboles", "value": 3, "priority": 2, "realValue": 3},
+    "030": {"literal": "Cuatro de Treboles", "value": 4, "priority": 2, "realValue": 4},
+    "031": {"literal": "Cinco de Treboles", "value": 5, "priority": 2, "realValue": 5},
+    "032": {"literal": "Seis de Treboles", "value": 6, "priority": 2, "realValue": 6},
+    "033": {"literal": "Siete de Treboles", "value": 7, "priority": 2, "realValue": 7},
+    "034": {"literal": "Ocho de Treboles", "value": 8, "priority": 2, "realValue": 0.5},
+    "035": {"literal": "Nueve de Treboles", "value": 9, "priority": 2, "realValue": 0.5},
+    "036": {"literal": "Diez de Treboles", "value": 10, "priority": 2, "realValue": 0.5},
+    "037": {"literal": "Sota de Treboles", "value": 11, "priority": 2, "realValue": 0.5},
+    "038": {"literal": "Reina de Treboles", "value": 12, "priority": 2, "realValue": 0.5},
+    "039": {"literal": "Rey de Treboles", "value": 13, "priority": 2, "realValue": 0.5},
+
+    "040": {"literal": "As de Picas", "value": 1, "priority": 1, "realValue": 1}, #Picas
+    "041": {"literal": "Dos de Picas", "value": 2, "priority": 1, "realValue": 2},
+    "042": {"literal": "Tres de Picas", "value": 3, "priority": 1, "realValue": 3},
+    "043": {"literal": "Cuatro de Picas", "value": 4, "priority": 1, "realValue": 4},
+    "044": {"literal": "Cinco de Picas", "value": 5, "priority": 1, "realValue": 5},
+    "045": {"literal": "Seis de Picas", "value": 6, "priority": 1, "realValue": 6},
+    "046": {"literal": "Siete de Picas", "value": 7, "priority": 1, "realValue": 7},
+    "047": {"literal": "Ocho de Picas", "value": 8, "priority": 1, "realValue": 0.5},
+    "048": {"literal": "Nueve de Picas", "value": 9, "priority": 1, "realValue": 0.5},
+    "049": {"literal": "Diez de Picas", "value": 10, "priority": 1, "realValue": 0.5},
+    "050": {"literal": "Sota de Picas", "value": 11, "priority": 1, "realValue": 0.5},
+    "051": {"literal": "Reina de Picas", "value": 12, "priority": 1, "realValue": 0.5},
+    "052": {"literal": "Rey de Picas", "value": 13, "priority": 1, "realValue": 0.5}
+
+}
+
+mazos = [esp48, esp40, poker]
