f17c07b06f56e9af0d0b6398b0aa22131f92b931
alejandroenti <alejandro.lopez@enti.cat>
2025-01-08 19:06:49 +0100

    Añadiendo función para imprimir el marco del Gameplay, añadiendo el turno y el jugador que está jugando en el borde inderior del marco

--- Programacion/printing.py
+++ Programacion/printing.py
@@ -1,7 +1,7 @@
 def print_title(title, padding = 0, fill_char = '*'):
     """
     Generamos el marco estándar para imprimir los títulos de las diferentes pantallas
-    :param title: (string) -> String del título en formato ASCII Arta
+    :param title: (string) -> String del título en formato ASCII Art
     :param padding: (int) -> Medida máxima horizontal que debe tener el título
     :param fill_char: (char) -> Carácter con el que se completará el padding
     :return: None
@@ -21,6 +21,32 @@ def print_title(title, padding = 0, fill_char = '*'):
 
     # Imprimimos el límite inferior
     print('\n' + ''.center(padding, fill_char) + '\n')
+
+def print_title_game(title, padding=0, fill_char='*', round=0, player_turn="Placeholder"):
+    """
+    Generamos el marco específico para imprimir el título en partida
+    :param title: (string) -> String del título en formato ASCII Arta
+    :param padding: (int) -> Medida máxima horizontal que debe tener el título
+    :param fill_char: (char) -> Carácter con el que se completará el padding
+    :param round: (int) -> Nos indica el número de ronda en el que nos encontramos
+    :param player_turn: (string) -> Nos indica el nombre del jugador que está realizando la tirada
+    :return: None
+    """
+    # Imprimimos el límite superior
+    print(''.center(padding, fill_char))
+
+    # Iteramos por cada una de las líneas del texto en modo ASCII para centrar el texto
+    line = ""
+    for char in title:
+        if char == '\n':
+            line = line.center(padding)
+            print(line)
+            line = ""
+            continue
+        line += char
+
+    # Imprimimos el límite inferior
+    print('\n' + f' Round {round}, Turn of {player_turn} '.center(padding, fill_char) + '\n')
 
 def print_line(text, padding = 0, fill_char = '*'):
     """
