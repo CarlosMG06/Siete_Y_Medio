613c80e50f7713ca35509629fb4a059138b1d5d3
alejandroenti <alejandro.lopez@enti.cat>
2025-01-17 17:21:40 +0100

    Acabado tutorial de simualar una jugada

--- Marcas/tutorial.js
+++ Marcas/tutorial.js
@@ -17,52 +17,250 @@ const setBetBtn = document.querySelector("#btn-submit-bet")
 const demandCardBtn = document.querySelector("#btn-demand-card")
 const standBtn = document.querySelector("#btn-stand")
 const betInput = document.querySelector("#input-bet")
-const playerCardsDict = [
+const cardDeck = [
     {
-        "src": "images/tutorial01_card01.avif",
-        "name": "12 de Espadas",
-        "value": 0.5
-    },
-    {
-        "src": "images/tutorial01_card02.avif",
-        "name": "1 de Copas",
-        "value": 1
-    }
-]
-const bankCardsDict = [
-    {
-        "src": "images/tutorial01_card01.avif",
-        "name": "12 de Espadas",
-        "value": 0.5
-    },
-    {
-        "src": "images/tutorial01_card02.avif",
-        "name": "1 de Copas",
+        "src": "images/carta_01_oros.avif",
+        "name": "1 de Oros",
         "value": 1
     },
     {
-        "src": "images/tutorial01_card01.avif",
-        "name": "12 de Espadas",
+        "src": "images/carta_02_oros.avif",
+        "name": "2 de Oros",
+        "value": 2
+    },
+    {
+        "src": "images/carta_03_oros.avif",
+        "name": "3 de Oros",
+        "value": 3
+    },
+    {
+        "src": "images/carta_04_oros.avif",
+        "name": "4 de Oros",
+        "value": 4
+    },
+    {
+        "src": "images/carta_05_oros.avif",
+        "name": "5 de Oros",
+        "value": 5
+    },
+    {
+        "src": "images/carta_06_oros.avif",
+        "name": "6 de Oros",
+        "value": 6
+    },
+    {
+        "src": "images/carta_07_oros.avif",
+        "name": "7 de Oros",
+        "value": 7
+    },
+    {
+        "src": "images/carta_08_oros.avif",
+        "name": "8 de Oros",
         "value": 0.5
     },
     {
-        "src": "images/tutorial01_card02.avif",
+        "src": "images/carta_09_oros.avif",
+        "name": "9 de Oros",
+        "value": 0.5
+    },
+    {
+        "src": "images/carta_10_oros.avif",
+        "name": "10 de Oros",
+        "value": 0.5
+    },
+    {
+        "src": "images/carta_11_oros.avif",
+        "name": "11 de Oros",
+        "value": 0.5
+    },
+    {
+        "src": "images/carta_12_oros.avif",
+        "name": "12 de Oros",
+        "value": 0.5
+    },
+    {
+        "src": "images/carta_01_copas.avif",
         "name": "1 de Copas",
         "value": 1
     },
     {
-        "src": "images/tutorial01_card01.avif",
-        "name": "12 de Espadas",
+        "src": "images/carta_02_copas.avif",
+        "name": "2 de Copas",
+        "value": 2
+    },
+    {
+        "src": "images/carta_03_copas.avif",
+        "name": "3 de Copas",
+        "value": 3
+    },
+    {
+        "src": "images/carta_04_copas.avif",
+        "name": "4 de Copas",
+        "value": 4
+    },
+    {
+        "src": "images/carta_05_copas.avif",
+        "name": "5 de Copas",
+        "value": 5
+    },
+    {
+        "src": "images/carta_06_copas.avif",
+        "name": "6 de Copas",
+        "value": 6
+    },
+    {
+        "src": "images/carta_07_copas.avif",
+        "name": "7 de Copas",
+        "value": 7
+    },
+    {
+        "src": "images/carta_08_copas.avif",
+        "name": "8 de Copas",
         "value": 0.5
     },
     {
-        "src": "images/tutorial01_card02.avif",
-        "name": "1 de Copas",
+        "src": "images/carta_09_copas.avif",
+        "name": "9 de Copas",
+        "value": 0.5
+    },
+    {
+        "src": "images/carta_10_copas.avif",
+        "name": "10 de Copas",
+        "value": 0.5
+    },
+    {
+        "src": "images/carta_11_copas.avif",
+        "name": "11 de Copas",
+        "value": 0.5
+    },
+    {
+        "src": "images/carta_12_copas.avif",
+        "name": "12 de Copas",
+        "value": 0.5
+    },
+    {
+        "src": "images/carta_01_bastos.avif",
+        "name": "1 de Bastos",
         "value": 1
+    },
+    {
+        "src": "images/carta_02_bastos.avif",
+        "name": "2 de Bastos",
+        "value": 2
+    },
+    {
+        "src": "images/carta_03_bastos.avif",
+        "name": "3 de Bastos",
+        "value": 3
+    },
+    {
+        "src": "images/carta_04_bastos.avif",
+        "name": "4 de Bastos",
+        "value": 4
+    },
+    {
+        "src": "images/carta_05_bastos.avif",
+        "name": "5 de Bastos",
+        "value": 5
+    },
+    {
+        "src": "images/carta_06_bastos.avif",
+        "name": "6 de Bastos",
+        "value": 6
+    },
+    {
+        "src": "images/carta_07_bastos.avif",
+        "name": "7 de Bastos",
+        "value": 7
+    },
+    {
+        "src": "images/carta_08_bastos.avif",
+        "name": "8 de Bastos",
+        "value": 0.5
+    },
+    {
+        "src": "images/carta_09_bastos.avif",
+        "name": "9 de Bastos",
+        "value": 0.5
+    },
+    {
+        "src": "images/carta_10_bastos.avif",
+        "name": "10 de Bastos",
+        "value": 0.5
+    },
+    {
+        "src": "images/carta_11_bastos.avif",
+        "name": "11 de Bastos",
+        "value": 0.5
+    },
+    {
+        "src": "images/carta_12_bastos.avif",
+        "name": "12 de Bastos",
+        "value": 0.5
+    },
+    {
+        "src": "images/carta_01_espadas.avif",
+        "name": "1 de Espadas",
+        "value": 1
+    },
+    {
+        "src": "images/carta_02_espadas.avif",
+        "name": "2 de Espadas",
+        "value": 2
+    },
+    {
+        "src": "images/carta_03_espadas.avif",
+        "name": "3 de Espadas",
+        "value": 3
+    },
+    {
+        "src": "images/carta_04_espadas.avif",
+        "name": "4 de Espadas",
+        "value": 4
+    },
+    {
+        "src": "images/carta_05_espadas.avif",
+        "name": "5 de Espadas",
+        "value": 5
+    },
+    {
+        "src": "images/carta_06_espadas.avif",
+        "name": "6 de Espadas",
+        "value": 6
+    },
+    {
+        "src": "images/carta_07_espadas.avif",
+        "name": "7 de Espadas",
+        "value": 7
+    },
+    {
+        "src": "images/carta_08_espadas.avif",
+        "name": "8 de Espadas",
+        "value": 0.5
+    },
+    {
+        "src": "images/carta_09_espadas.avif",
+        "name": "9 de Espadas",
+        "value": 0.5
+    },
+    {
+        "src": "images/carta_10_espadas.avif",
+        "name": "10 de Espadas",
+        "value": 0.5
+    },
+    {
+        "src": "images/carta_11_espadas.avif",
+        "name": "11 de Espadas",
+        "value": 0.5
+    },
+    {
+        "src": "images/carta_12_espadas.avif",
+        "name": "12 de Espadas",
+        "value": 0.5
     }
 ]
 
-let cardIndex = 0
+let cardsCopy = []
 let points = 0
 
 const Delay = ms => new Promise(res => setTimeout(res, ms));
@@ -96,29 +294,30 @@ const PlayTutorialPart1 = () => {
 const PlayTutorialPart2 = async () => {
     insertBetForm.style.visibility = "hidden"
 
-    ShowCardTutorial2(cardIndex, playerCardsDict, playerScores)
+    cardsCopy = [...cardDeck]
+    cardToGet = GetRandomNumber(0, cardsCopy.length - 1)
+    ShowCardTutorial2(cardToGet, cardsCopy, playerScores)
 
     await Delay(500)
     demandCardForm.style.visibility = "visible"
-    cardIndex++
+    cardsCopy.splice(cardToGet, 1)
 }
 
 const PlayTutorialPart3 = async () => {
-    if (cardIndex > playerCardsDict.length || cardIndex === playerCardsDict.length) {
+    if (parseFloat(playerScores[1]) > 7.5) {
         demandCardForm.style.visibility = "hidden"
         msgText.innerHTML = "¡Te has pasado!"
         msgDiv.style.visibility = "visible"
         playerScores[0].innerHTML = parseInt(playerScores[0].innerHTML) - parseInt(playerScores[2].innerHTML)
-        bankScoresScores[0].innerHTML = parseInt(playerScores[2].innerHTML)
-        playerScores[1].innerHTML = 0
-        bankScores[1].innerHTML = 0
+        bankScores[0].innerHTML = parseInt(bankScores[0].innerHTML) + parseInt(playerScores[2].innerHTML)
         return 
     }
 
-    ShowCardTutorial2(cardIndex, playerCardsDict, playerScores)
+    cardToGet = GetRandomNumber(0, cardsCopy.length - 1)
+    ShowCardTutorial2(cardToGet, cardsCopy, playerScores)
     await Delay(500)
     demandCardForm.style.visibility = "visible"
-    cardIndex++
+    cardsCopy.splice(cardToGet, 1)
 }
 
 const PlayTutorialPart4 = async () => {
@@ -127,52 +326,63 @@ const PlayTutorialPart4 = async () => {
 
     msgText.innerHTML = "Juga la banca"
     msgDiv.style.visibility = "visible"
-    await Delay(5000)
+    await Delay(1500)
     msgDiv.style.visibility = "hidden"
 
-    // Randomizamos el número de cartas que va a recoger la banca de su lado
-    cardToGet = GetRandomNumber(1, bankCardsDict.length - 1)
     points = 0
-    ShowCardTutorial2(0, bankCardsDict, bankScores)
-    for (let index = 1; index < cardToGet; index++) {
-        if (parseFloat(bankScores[1].innerHTML) === 7.5 || parseFloat(bankScores[1].innerHTML) > 7.5 || parseFloat(bankScores[1].innerHTML) > parseFloat(playerScores[1].innerHTML) || parseFloat(bankScores[1].innerHTML) === parseFloat(playerScores[1].innerHTML )) {
-            break
-        }
-        await Delay(2000)
-        ShowCardTutorial2(index, bankCardsDict, bankScores)
+    cardsCopy = [...cardDeck]
+    while (parseFloat(bankScores[1].innerHTML) < 7.5 && parseFloat(bankScores[1].innerHTML) < parseFloat(playerScores[1].innerHTML) && cardsCopy.length > 0) {
+        await Delay(1500)
+        cardImg.style.visibility = "hidden"
+        cardName.style.visibility = "hidden"
+        await Delay(1000)
+        cardToGet = GetRandomNumber(0, cardsCopy.length - 1)
+        ShowCardTutorial2(cardToGet, cardsCopy, bankScores)
+        cardsCopy.splice(cardToGet, 1)
     }
 
     if (parseFloat(bankScores[1].innerHTML) < parseFloat(playerScores[1].innerHTML)) {
         msgText.innerHTML = "¡Has ganado!"
+        playerScores[0].innerHTML = parseInt(playerScores[0].innerHTML) + Math.min(parseInt(playerScores[2].innerHTML), parseInt(bankScores[0].innerHTML))
         bankScores[0].innerHTML = parseInt(bankScores[0].innerHTML) - parseInt(playerScores[2].innerHTML)
-        playerScores[0].innerHTML = parseInt(playerScores[0].innerHTML) + parseInt(playerScores[2].innerHTML)
-        playerScores[1].innerHTML = 0
-        bankScores[1].innerHTML = 0
+        
     }
-    else if (parseFloat(playerScores[1].innerHTML) == 7.5 && parseFloat(bankScores[1].innerHTML) != 7.5) {
+    else if (parseFloat(playerScores[1].innerHTML) === 7.5 && parseFloat(bankScores[1].innerHTML) != 7.5) {
         msgText.innerHTML = "¡Has ganado! Eres candidato a la banca"
+        playerScores[0].innerHTML = parseInt(playerScores[0].innerHTML) + Math.min(parseInt(playerScores[2].innerHTML), parseInt(bankScores[0].innerHTML))
         bankScores[0].innerHTML = parseInt(bankScores[0].innerHTML) - parseInt(playerScores[2].innerHTML)
-        playerScores[0].innerHTML = parseInt(playerScores[0].innerHTML) + parseInt(playerScores[2].innerHTML)
-        playerScores[1].innerHTML = 0
-        bankScores[1].innerHTML = 0
     }
     else if (parseFloat(bankScores[1].innerHTML) === parseFloat(playerScores[1].innerHTML)) {
-        console.log("Empate")
         msgText.innerHTML = "¡Gana la banca por empate!"
         playerScores[0].innerHTML = parseInt(playerScores[0].innerHTML) - parseInt(playerScores[2].innerHTML)
-        bankScoresScores[0].innerHTML = parseInt(bankScoresScores[0].innerHTML) + parseInt(playerScores[2].innerHTML)
-        playerScores[1].innerHTML = 0
-        bankScores[1].innerHTML = 0
+        bankScores[0].innerHTML = parseInt(bankScores[0].innerHTML) + parseInt(playerScores[2].innerHTML)
     }
-    else if (parseFloat(bankScores[1].innerHTML) === 7.5 || parseFloat(bankScores[1].innerHTML) < 7.5 && parseFloat(bankScores[1].innerHTML) > parseFloat(playerScores[1].innerHTML)) {
+    else if (parseFloat(bankScores[1].innerHTML) === 7.5 || (parseFloat(bankScores[1].innerHTML) < 7.5 && parseFloat(bankScores[1].innerHTML) > parseFloat(playerScores[1].innerHTML))) {
         msgText.innerHTML = "¡Gana la banca!"
         playerScores[0].innerHTML = parseInt(playerScores[0].innerHTML) - parseInt(playerScores[1].innerHTML)
-        bankScoresScores[0].innerHTML = parseInt(bankScoresScores[0].innerHTML) + parseInt(playerScores[2].innerHTML)
-        playerScores[1].innerHTML = 0
-        bankScores[1].innerHTML = 0
+        bankScores[0].innerHTML = parseInt(bankScores[0].innerHTML) + parseInt(playerScores[2].innerHTML)
     }
     playerScores[2].innerHTML = 0
     msgDiv.style.visibility = "visible"
+}
+
+const ResetPlayTutorial = () => {
+    bankScores[0].innerHTML = 15
+    bankScores[1].innerHTML = 0
+    playerScores[0].innerHTML = 20
+    playerScores[1].innerHTML = 0
+    playerScores[2].innerHTML = 0
+
+    cardImg.style.visibility = "hidden"
+    cardName.style.visibility = "hidden"
+
+    msgText.innerHTML = ""
+    msgDiv.style.visibility = "hidden"
+
+    demandCardForm.style.visibility = "hidden"
+    insertBetForm.style.visibility = "hidden"
+
+    points = 0
 }
 
 const ShowCardTutorial2 = (cardIndex, dict, scores) => {
@@ -192,7 +402,7 @@ const HideCardTutorial2 = () => {
 }
 
 const GetRandomNumber = (min, max) => {
-    return Math.random() * (max - min) + min
+    return Math.floor(Math.random() * (max - min) + min)
 }
 
 setBetBtn.addEventListener("click", async (event) => {
